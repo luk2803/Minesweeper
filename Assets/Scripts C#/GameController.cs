58ce33213d83a6d30f194b69dec785e0503a9be9
BuildTools <unconfigured@null.spigotmc.org>
2019-07-19 20:35:23 +0200

    Spielprinzip fertig

---
+++ Assets/Scripts C#/GameController.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+using UnityEditor.Profiling.Memory.Experimental;
+using UnityEngine;
+using UnityEngine.Analytics;
+using Random = System.Random;
+
+public class GameController : MonoBehaviour
+{
+    public bool gameOver = false;
+    private Sprite ownSprite;
+    public Sprite mineClicked;
+    public Sprite mineOpen;
+    public Sprite mine;
+    public Sprite openbomb;
+    public Sprite openredbomb;
+    public Sprite openmine_1;
+    public Sprite openmine_2;
+    public Sprite openmine_3;
+    public Sprite openmine_4;
+    public Sprite openmine_5;
+    private Mine[,] spielfeld;
+    private Transform transform;
+    private GameObject gameController;
+    public bool Alreadyclicked { get; set; } = false;
+    public int anzmines;
+    static Random rnd = new Random();
+    public int length;
+    public int spielzüge=0;
+    public bool gewonnen = false;
+    public bool message = false;
+
+    public Mine[,] GetSpielFeld()
+    {
+        return spielfeld;
+    }
+
+    public bool GetMinePosIJ(Mine m, out int i, out int j)
+    {
+        j = 0;
+
+        for (i = 0; i < length; i++)
+        {
+            for (j = 0; j < length; j++)
+            {
+                if (Mine.Compare(spielfeld[i, j], m))
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public void openBombs()
+    {
+        foreach (var VARIABLE in spielfeld)
+        {
+            if (VARIABLE.IsMine)
+            {
+                VARIABLE.SetOwnSprite(openbomb);
+             
+            }
+
+        }
+    }
+
+    public int isbomb(Mine m)
+    {      
+            if (m.IsMine)
+                return 1;          
+            return 0;    
+    }
+    public void SetNumbers()
+    {
+        for (int i = 0; i < length; i++)
+        {
+            for (int j = 0; j < length; j++)
+            {
+                if (spielfeld[i, j].IsMine)
+                {               
+                    continue;
+                }
+
+                try
+                {
+                    spielfeld[i, j].minesinnear += isbomb(spielfeld[i + 1, j]);
+                }
+                catch
+                {   }
+                try
+                {
+                    spielfeld[i, j].minesinnear += isbomb(spielfeld[i -1, j]);
+                }
+                catch {  }
+                try
+                {
+                    spielfeld[i, j].minesinnear += isbomb(spielfeld[i +1, j+1]);
+                }
+                catch {  }
+                try
+                {
+                    spielfeld[i, j].minesinnear += isbomb(spielfeld[i -1, j-1]);
+                }
+                catch {  }
+                try
+                {
+                    spielfeld[i, j].minesinnear += isbomb(spielfeld[i +1, j-1]);
+                }
+                catch {}
+                try
+                {
+                    spielfeld[i, j].minesinnear += isbomb(spielfeld[i -1, j+1]);
+                }
+                catch { }
+                try
+                {
+                    spielfeld[i, j].minesinnear += isbomb(spielfeld[i, j+1]);
+                }
+                catch { }
+                try
+                {
+                    spielfeld[i, j].minesinnear += isbomb(spielfeld[i, j-1]);
+                }
+                catch { }
+                
+
+            }
+        }
+    }
+
+    public void resetmines()
+    {
+        foreach (var VARIABLE in spielfeld)
+        {
+
+            VARIABLE.IsMine = false;
+            VARIABLE.SetOwnSprite(mine);
+            VARIABLE.minesinnear = 0;
+        }
+    }
+
+    public Mine GetMine(int pos)
+    {
+        int anz = 1;
+        for (int i = 0; i <length ; i++)
+        {
+            for (int j = 0; j < length; j++)
+            {
+                if (anz == pos)
+                    return spielfeld[i, j];
+                anz++;
+            }
+        }
+
+        return null;
+    }
+
+    public void GameStart()
+    {
+
+        spielzüge = 0;
+            int c = spielfeld.Length;
+            resetmines();
+            for (int i = 0; i < anzmines; i++)
+            {
+                Mine m;
+                if (((m = GetMine(rnd.Next(1, c+1)))).IsMine)
+                {
+                    i--;
+                    continue;
+                }
+                else
+                    m.SetIsMine();
+
+            }
+
+            SetNumbers();
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameStart();
+
+    }
+
+
+    void Awake()
+    {
+
+        gameController= this.gameObject;
+    //alle Minen in eine Liste packen dann in 2d array hauen. wurzel aus anz ziehen und länge festlegen.
+     List<GameObject> allmines = new List<GameObject>();
+     transform = gameController.transform;
+     
+     for (int i = 0; i < transform.childCount; i++)
+     {
+        allmines.Add(transform.GetChild(i).gameObject);
+     }
+     Debug.Log(allmines.Count +" allmines");
+     Debug.Log(transform.childCount+ " childcount");
+    
+
+        length = (int)Math.Sqrt(allmines.Count);
+     spielfeld= new Mine[length,length];
+     int c = 0;
+     for (int i = 0; i < length; i++)
+     {
+         for (int j = 0; j < length; j++)
+         {
+            
+             spielfeld[i, j] = allmines[c].GetComponent<Mine>(); ;
+             spielfeld[i, j].position = c;
+             spielfeld[i,j].SetOwnSprite(mine);
+                 c++;
+         }
+     }
+
+    
+
+    }
+
+
+// Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts C#/Mine.cs
@@ -0,0 +1,400 @@
+﻿using UnityEngine;
+//using GameController;
+
+public class Mine : MonoBehaviour
+{
+    public enum Mine_State
+    {
+        one = 1,
+        two,
+        three,
+        four,
+        five,
+        six,
+        seven,
+        eight,
+        clicked,
+        open,
+        mine,
+        bomb,
+        redbomb
+
+
+    }
+
+    public int position;
+    public bool IsMine { get; set; } = false;
+
+    private BoxCollider2D boxCollider;
+    public int minesinnear = 0;
+    private Sprite openbomb;
+    private Sprite openredbomb;
+    private Sprite mineClicked;
+    private Sprite mineOpen;
+    private Sprite mine;
+    private Sprite openmine_1;
+    private Sprite openmine_2;
+    private Sprite openmine_3;
+    private Sprite openmine_4;
+    private Sprite openmine_5;
+
+    private GameObject gameController;
+    private GameController controllerScript;
+    private bool messagenear = false;
+
+    //alle publics mit gettern/settern ersetzen 
+    //algorithmus erstellen
+    //bilder für zahlen erstellen
+    //bombem filtern die nicht herausfindbar sind
+    //start/endscreen
+    //selbsteintscheidung von größe des feldes und anz der bomben
+
+    public Mine_State state = Mine_State.mine;
+    private bool exit = false;
+
+    private Mine m = null;
+    private bool first = true;
+
+    public void Fill(int i, int j)
+    {
+
+        if (first)
+        {
+            m.Click();
+            if (minesinnear != 0)
+                return;
+
+            first = false;
+        }
+        else
+        {
+            try
+            {
+                m = controllerScript.GetSpielFeld()[i, j];
+            }
+            catch
+            {
+                return;
+            }
+
+            if (m == null)
+            {
+                return;
+            }
+
+            if (m.state != Mine_State.mine)
+            {
+
+                return;
+            }
+
+            m.Click();
+            if (m.minesinnear != 0)
+            {
+
+                return;
+            }
+        }
+
+        Fill(i + 1, j);
+        Fill(i, j + 1);
+        Fill(i - 1, j);
+        Fill(i, j - 1);
+
+
+    }
+
+    public static bool Compare(Mine m, Mine mine)
+    {
+        if (m.name == mine.name)
+            return true;
+        return false;
+    }
+
+    public void SetOwnSprite(int number)
+    {
+
+
+        switch (number)
+        {
+            case 0:
+                {
+                    state = Mine_State.open;
+
+                    this.GetComponent<SpriteRenderer>().sprite = mineOpen;
+                    break;
+                }
+            case 1:
+                {
+                    state = Mine_State.one;
+                    this.GetComponent<SpriteRenderer>().sprite = openmine_1;
+                    break;
+                }
+            case 2:
+                {
+                    state = Mine_State.two;
+                    this.GetComponent<SpriteRenderer>().sprite = openmine_2;
+                    break;
+                }
+            case 3:
+                {
+                    state = Mine_State.three;
+                    this.GetComponent<SpriteRenderer>().sprite = openmine_3;
+                    break;
+                }
+            case 4:
+                {
+                    state = Mine_State.four;
+                    this.GetComponent<SpriteRenderer>().sprite = openmine_4;
+                    break;
+                }
+            case 5:
+                {
+                    state = Mine_State.five;
+                    this.GetComponent<SpriteRenderer>().sprite = openmine_5;
+                    break;
+                }
+        }
+    }
+
+    public void SetOwnSprite(Sprite mine)
+    {
+        this.GetComponent<SpriteRenderer>().sprite = mine;
+
+        switch (mine.name)
+        {
+            case "mine_open":
+                {
+
+                    state = Mine_State.open;
+                    break;
+                }
+            case "mine_clicked":
+                {
+
+                    state = Mine_State.clicked;
+                    break;
+                }
+            case "mine_closed":
+                {
+
+                    state = Mine_State.mine;
+                    break;
+                }
+            case "mineopenwithbomb":
+                {
+                    state = Mine_State.bomb;
+                    break;
+                }
+            case "redbomb":
+                {
+                    state = Mine_State.redbomb;
+                    break;
+                }
+        }
+    }
+
+    public bool Click()
+    {
+        
+        if (!IsMine)
+        {
+            if (minesinnear != 0 && controllerScript.spielzüge == 0)
+            {
+                controllerScript.GameStart();
+                Debug.Log("Neustart!");
+                int i, j;
+                controllerScript.GetMinePosIJ(this, out i, out j);
+                Fill(i, j);
+                return false;
+            }
+            controllerScript.spielzüge++;
+
+            SetOwnSprite(minesinnear);
+            Debug.Log(controllerScript.spielzüge);
+            Debug.Log(controllerScript.GetSpielFeld().Length - controllerScript.anzmines);
+
+            if (controllerScript.spielzüge ==
+                controllerScript.GetSpielFeld().Length - controllerScript.anzmines)
+            {
+                controllerScript.gewonnen = true;
+                controllerScript.gameOver = true;
+            }
+        }
+        else
+        {
+            if (controllerScript.spielzüge == 0)
+            {
+                controllerScript.GameStart();
+                Debug.Log("Neustart!");
+                int i, j;
+                controllerScript.GetMinePosIJ(this, out i, out j);
+                Fill(i, j);
+                return true;
+            }
+            controllerScript.gameOver = true;
+
+            controllerScript.openBombs();
+
+            SetOwnSprite(openredbomb);
+
+        }
+        //Debug.Log(controllerScript.spielzüge + " spielzüge");
+      
+        return true;
+    }
+
+    public void ProveClick()
+    {
+
+        if (controllerScript.gameOver)
+        {
+            if (controllerScript.gewonnen)
+            {
+                if (!controllerScript.message)
+                {
+                    Debug.Log("Gewonnen!");
+                    controllerScript.message = true;
+                }
+
+            }
+            return;
+        }
+
+        if (!messagenear)
+        {
+            //Debug.Log("mines in near: " + minesinnear);
+            messagenear = true;
+        }
+        if (Input.GetMouseButton(0))
+        {
+
+            if (controllerScript.Alreadyclicked)
+                return;
+            if (state == Mine_State.mine)
+            {
+                SetOwnSprite(mineClicked);
+
+                controllerScript.Alreadyclicked = true;
+            }
+        }
+        else
+        {
+
+            if (state == Mine_State.clicked)
+            {
+                if (!IsMine)
+                {
+
+                    int i, j;
+                    controllerScript.GetMinePosIJ(this, out i, out j);
+
+                    // Debug.Log("fill!!!!!!");
+                    Fill(i, j);
+                    //Click();
+
+
+                }
+                else
+                {
+                    if (controllerScript.spielzüge == 0)
+                    {
+                        controllerScript.GameStart();
+                        int i, j;
+                        controllerScript.GetMinePosIJ(this, out i, out j);
+                        Debug.Log("Neustart!");
+                        Fill(i, j);
+                        //Click();
+                        return;
+                    }
+                    controllerScript.gameOver = true;
+
+                    controllerScript.openBombs();
+
+                    SetOwnSprite(openredbomb);
+
+                }
+
+                controllerScript.Alreadyclicked = false;
+            }
+            else
+            {
+                controllerScript.Alreadyclicked = false;
+            }
+
+
+
+        }
+    }
+
+    void OnMouseOver()
+    {
+        ProveClick();
+
+    }
+
+    void OnMouseExit()
+    {
+        exit = true;
+        messagenear = false;
+
+    }
+
+    public void SetIsMine()
+    {
+        IsMine = true;
+    }
+
+
+    private void Awake()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+
+
+
+        gameController = GameObject.Find("GameController");
+        controllerScript = gameController.GetComponent<GameController>();
+        mineOpen = controllerScript.mineOpen;
+        mineClicked = controllerScript.mineClicked;
+        mine = controllerScript.mine;
+        openredbomb = controllerScript.openredbomb;
+        openbomb = controllerScript.openbomb;
+        openmine_1 = controllerScript.openmine_1;
+        openmine_2 = controllerScript.openmine_2;
+        openmine_3 = controllerScript.openmine_3;
+        openmine_4 = controllerScript.openmine_4;
+        openmine_5 = controllerScript.openmine_5;
+        m = this;
+
+
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (exit)
+        {
+            if (!Input.GetMouseButton(0))
+            {
+                exit = false;
+                if (state == Mine_State.clicked)
+                {
+                    SetOwnSprite(mine);
+
+                }
+            }
+            else
+            {
+                return;
+            }
+        }
+
+    }
+}
